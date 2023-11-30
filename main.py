a26dd1e11149f62bff689c86f84a446b9be46d83
anarond <anarond@gmail.com>
2023-11-30 15:00:11 +0300

    + while breakfast lunch dinner to dict_breakfast, dict_lunch, dict_dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-breakfast = {
+dict_breakfast = {
     'Каша1': {
         'cal': 111,
         'ingrid': ['манка', 'молоко']
@@ -31,7 +31,7 @@ breakfast = {
     }
 }
 
-lunch = {
+dict_lunch = {
     'Борщ2': {
         'cal': 444,
         'ingrid': ['свекла', 'картофель', 'морковь', 'лук', 'капуста']
@@ -62,7 +62,7 @@ lunch = {
     }
 }
 
-dinner = {
+dict_dinner = {
     'Шашлык3': {
         'cal': 444,
         'ingrid': ['мясо', 'лук', 'соус']
@@ -93,75 +93,89 @@ dinner = {
     }
 }
 
-monday_breakfast = random.choice(list(breakfast.keys()))
-monday_lunch = random.choice(list(lunch.keys()))
-monday_dinner = random.choice(list(dinner.keys()))
-print(monday_breakfast, monday_lunch, monday_dinner)
+i = 0
 
-del breakfast[monday_breakfast]
-del lunch[monday_lunch]
-del dinner[monday_dinner]
+while i != 7:
+    breakfast = random.choice(list(dict_breakfast.keys()))
+    lunch = random.choice(list(dict_lunch.keys()))
+    dinner = random.choice(list(dict_dinner.keys()))
+    print(breakfast, lunch, dinner)
+
+    del dict_breakfast[breakfast]
+    del dict_lunch[lunch]
+    del dict_dinner[dinner]
+    #print(breakfast, lunch, dinner)
+    i += 1
+
+#monday_breakfast = random.choice(list(breakfast.keys()))
+#monday_lunch = random.choice(list(lunch.keys()))
+#monday_dinner = random.choice(list(dinner.keys()))
+#print(monday_breakfast, monday_lunch, monday_dinner)
+
+#del breakfast[monday_breakfast]
+#del lunch[monday_lunch]
+#del dinner[monday_dinner]
 #print(breakfast, lunch, dinner)
 
-tuesday_breakfast = random.choice(list(breakfast.keys()))
-tuesday_lunch = random.choice(list(lunch.keys()))
-tuesday_dinner = random.choice(list(dinner.keys()))
-print(tuesday_breakfast, tuesday_lunch, tuesday_dinner)
+#tuesday_breakfast = random.choice(list(breakfast.keys()))
+#tuesday_lunch = random.choice(list(lunch.keys()))
+#tuesday_dinner = random.choice(list(dinner.keys()))
+#print(tuesday_breakfast, tuesday_lunch, tuesday_dinner)
 
-del breakfast[tuesday_breakfast]
-del lunch[tuesday_lunch]
-del dinner[tuesday_dinner]
+#del breakfast[tuesday_breakfast]
+#del lunch[tuesday_lunch]
+#del dinner[tuesday_dinner]
 #print(breakfast, lunch, dinner)
 
-wednesday_breakfast = random.choice(list(breakfast.keys()))
-wednesday_lunch = random.choice(list(lunch.keys()))
-wednesday_dinner = random.choice(list(dinner.keys()))
-print(wednesday_breakfast, wednesday_lunch, wednesday_dinner)
+#wednesday_breakfast = random.choice(list(breakfast.keys()))
+#wednesday_lunch = random.choice(list(lunch.keys()))
+#wednesday_dinner = random.choice(list(dinner.keys()))
+#print(wednesday_breakfast, wednesday_lunch, wednesday_dinner)
 
-del breakfast[wednesday_breakfast]
-del lunch[wednesday_lunch]
-del dinner[wednesday_dinner]
+#del breakfast[wednesday_breakfast]
+#del lunch[wednesday_lunch]
+#del dinner[wednesday_dinner]
 #print(breakfast, lunch, dinner)
 
-thursday_breakfast = random.choice(list(breakfast.keys()))
-thursday_lunch = random.choice(list(lunch.keys()))
-thursday_dinner = random.choice(list(dinner.keys()))
-print(thursday_breakfast, thursday_lunch, thursday_dinner)
+#thursday_breakfast = random.choice(list(breakfast.keys()))
+#thursday_lunch = random.choice(list(lunch.keys()))
+#thursday_dinner = random.choice(list(dinner.keys()))
+#print(thursday_breakfast, thursday_lunch, thursday_dinner)
 
-del breakfast[thursday_breakfast]
-del lunch[thursday_lunch]
-del dinner[thursday_dinner]
+#del breakfast[thursday_breakfast]
+#del lunch[thursday_lunch]
+#del dinner[thursday_dinner]
 #print(breakfast, lunch, dinner)
 
-friday_breakfast = random.choice(list(breakfast.keys()))
-friday_lunch = random.choice(list(lunch.keys()))
-friday_dinner = random.choice(list(dinner.keys()))
-print(friday_breakfast, friday_lunch, friday_dinner)
+#friday_breakfast = random.choice(list(breakfast.keys()))
+#friday_lunch = random.choice(list(lunch.keys()))
+#friday_dinner = random.choice(list(dinner.keys()))
+#print(friday_breakfast, friday_lunch, friday_dinner)
 
-del breakfast[friday_breakfast]
-del lunch[friday_lunch]
-del dinner[friday_dinner]
+#del breakfast[friday_breakfast]
+#del lunch[friday_lunch]
+#del dinner[friday_dinner]
 #print(breakfast, lunch, dinner)
 
-saturday_breakfast = random.choice(list(breakfast.keys()))
-saturday_lunch = random.choice(list(lunch.keys()))
-saturday_dinner = random.choice(list(dinner.keys()))
-print(saturday_breakfast, saturday_lunch, saturday_dinner)
+#saturday_breakfast = random.choice(list(breakfast.keys()))
+#saturday_lunch = random.choice(list(lunch.keys()))
+#saturday_dinner = random.choice(list(dinner.keys()))
+#print(saturday_breakfast, saturday_lunch, saturday_dinner)
 
-del breakfast[saturday_breakfast]
-del lunch[saturday_lunch]
-del dinner[saturday_dinner]
+#del breakfast[saturday_breakfast]
+#del lunch[saturday_lunch]
+#del dinner[saturday_dinner]
 #print(breakfast, lunch, dinner)
 
-sunday_breakfast = random.choice(list(breakfast.keys()))
-sunday_lunch = random.choice(list(lunch.keys()))
-sunday_dinner = random.choice(list(dinner.keys()))
-print(sunday_breakfast, sunday_lunch, sunday_dinner)
+#sunday_breakfast = random.choice(list(breakfast.keys()))
+#sunday_lunch = random.choice(list(lunch.keys()))
+#sunday_dinner = random.choice(list(dinner.keys()))
+#print(sunday_breakfast, sunday_lunch, sunday_dinner)
 
-del breakfast[sunday_breakfast]
-del lunch[sunday_lunch]
-del dinner[sunday_dinner]
-print(breakfast, lunch, dinner)
+#del breakfast[sunday_breakfast]
+#del lunch[sunday_lunch]
+#del dinner[sunday_dinner]
+#print(breakfast, lunch, dinner)
 
 # test push
 # test push win
