59a780395b2163b19a9194b20629cb494b73a079
anarond <anarond@gmail.com>
2023-12-01 12:12:43 +0300

    + выборка блюд по калориям - лишний код

--- main.py
+++ main.py
@@ -93,89 +93,41 @@ dict_dinner = {
     }
 }
 
-i = 0
-
-while i != 7:
+breakfast = random.choice(list(dict_breakfast.keys()))
+cal_breakfast = dict_breakfast.get(breakfast)['cal']
+while cal_breakfast > 200:
     breakfast = random.choice(list(dict_breakfast.keys()))
+    cal_breakfast = dict_breakfast[breakfast]['cal']
+print(breakfast)
+
+lunch = random.choice(list(dict_lunch.keys()))
+cal_lunch = dict_lunch.get(lunch)['cal']
+while cal_lunch > 500:
     lunch = random.choice(list(dict_lunch.keys()))
+    cal_lunch = dict_lunch[lunch]['cal']
+print(lunch)
+
+dinner = random.choice(list(dict_dinner.keys()))
+cal_dinner = dict_dinner.get(dinner)['cal']
+while cal_dinner > 400:
     dinner = random.choice(list(dict_dinner.keys()))
-    print(breakfast, lunch, dinner)
+    cal_dinner = dict_dinner[dinner]['cal']
+print(dinner)
 
-    del dict_breakfast[breakfast]
-    del dict_lunch[lunch]
-    del dict_dinner[dinner]
-    #print(breakfast, lunch, dinner)
-    i += 1
 
-#monday_breakfast = random.choice(list(breakfast.keys()))
-#monday_lunch = random.choice(list(lunch.keys()))
-#monday_dinner = random.choice(list(dinner.keys()))
-#print(monday_breakfast, monday_lunch, monday_dinner)
+# i = 0
 
-#del breakfast[monday_breakfast]
-#del lunch[monday_lunch]
-#del dinner[monday_dinner]
-#print(breakfast, lunch, dinner)
+# while i != 7:
+#    breakfast = random.choice(list(dict_breakfast.keys()))
+#    lunch = random.choice(list(dict_lunch.keys()))
+#    dinner = random.choice(list(dict_dinner.keys()))
+#    print(breakfast, lunch, dinner)
+#        if breakfast
+#        else
+#            del dict_breakfast[breakfast]
+#            del dict_lunch[lunch]
+#            del dict_dinner[dinner]
+#            #print(breakfast, lunch, dinner)
+#            i += 1
 
-#tuesday_breakfast = random.choice(list(breakfast.keys()))
-#tuesday_lunch = random.choice(list(lunch.keys()))
-#tuesday_dinner = random.choice(list(dinner.keys()))
-#print(tuesday_breakfast, tuesday_lunch, tuesday_dinner)
 
-#del breakfast[tuesday_breakfast]
-#del lunch[tuesday_lunch]
-#del dinner[tuesday_dinner]
-#print(breakfast, lunch, dinner)
-
-#wednesday_breakfast = random.choice(list(breakfast.keys()))
-#wednesday_lunch = random.choice(list(lunch.keys()))
-#wednesday_dinner = random.choice(list(dinner.keys()))
-#print(wednesday_breakfast, wednesday_lunch, wednesday_dinner)
-
-#del breakfast[wednesday_breakfast]
-#del lunch[wednesday_lunch]
-#del dinner[wednesday_dinner]
-#print(breakfast, lunch, dinner)
-
-#thursday_breakfast = random.choice(list(breakfast.keys()))
-#thursday_lunch = random.choice(list(lunch.keys()))
-#thursday_dinner = random.choice(list(dinner.keys()))
-#print(thursday_breakfast, thursday_lunch, thursday_dinner)
-
-#del breakfast[thursday_breakfast]
-#del lunch[thursday_lunch]
-#del dinner[thursday_dinner]
-#print(breakfast, lunch, dinner)
-
-#friday_breakfast = random.choice(list(breakfast.keys()))
-#friday_lunch = random.choice(list(lunch.keys()))
-#friday_dinner = random.choice(list(dinner.keys()))
-#print(friday_breakfast, friday_lunch, friday_dinner)
-
-#del breakfast[friday_breakfast]
-#del lunch[friday_lunch]
-#del dinner[friday_dinner]
-#print(breakfast, lunch, dinner)
-
-#saturday_breakfast = random.choice(list(breakfast.keys()))
-#saturday_lunch = random.choice(list(lunch.keys()))
-#saturday_dinner = random.choice(list(dinner.keys()))
-#print(saturday_breakfast, saturday_lunch, saturday_dinner)
-
-#del breakfast[saturday_breakfast]
-#del lunch[saturday_lunch]
-#del dinner[saturday_dinner]
-#print(breakfast, lunch, dinner)
-
-#sunday_breakfast = random.choice(list(breakfast.keys()))
-#sunday_lunch = random.choice(list(lunch.keys()))
-#sunday_dinner = random.choice(list(dinner.keys()))
-#print(sunday_breakfast, sunday_lunch, sunday_dinner)
-
-#del breakfast[sunday_breakfast]
-#del lunch[sunday_lunch]
-#del dinner[sunday_dinner]
-#print(breakfast, lunch, dinner)
-
-# test push
-# test push win
